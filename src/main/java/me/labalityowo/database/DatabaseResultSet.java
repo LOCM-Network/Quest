2251dfff34857b7b7dc89f033ac698c7e0624de1
Labality <56186498+Labality@users.noreply.github.com>
2021-10-13 17:08:30 +0700

    Delete src/main directory

--- src/main/java/me/labalityowo/database/DatabaseResultSet.java
+++
@@ -1,9 +0,0 @@
-package me.labalityowo.database;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-public interface DatabaseResultSet {
-
-    public void processData(ResultSet resultSet) throws SQLException;
-}
--- src/main/java/me/labalityowo/quests/type/BreakQuest.java
+++
@@ -1,9 +0,0 @@
-package me.labalityowo.quests.type;
-
-import cn.nukkit.block.Block;
-
-public class BreakQuest extends BlockQuest{
-    public BreakQuest(int progress, int maxProgress, boolean rewardCollected, Block block) {
-        super(progress, maxProgress, rewardCollected, block);
-    }
-}
